9f868928ae65da9b860b837c38da185ef15f50cd
Mithil Menon <mithilmenon11@gmail.com>
2025-04-22 17:33:10 +0530

    MongoDB Project

--- MegaProject-MongoDB/02_express/index.js
+++ MegaProject-MongoDB/02_express/index.js
@@ -1,57 +1,72 @@
-import express from 'express'
+import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
-const app=express()
-const port =3000
-app.use(express.json())
+const app = express();
+const port = 3000;
+app.use(express.json());
 
-let teaData=[]
-let nextID=1
+const morganFormat = ":method :url :status :response-time ms";
 
-app.post("/teas",(req,res)=>{
-    const {name,price}=req.body
-    const newTea={id:nextID++,name,price}
-    teaData.push(newTea)
-    res.status(201).send(newTea)
-})
+//Middleware
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
-app.get('/teas',(req,res)=>{
-    res.status(200).send(teaData)
-})
-app.get('/teas/:id',(req,res)=>{
-    const tea=(teaData.find(t=> t.id=== parseInt(req.params.id)))
-    if (!tea) {
-        return res.status(404).send('Tea not found')
-    }
-    res.status(200).send(tea);
-})
+let teaData = [];
+let nextID = 1;
 
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = { id: nextID++, name, price };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
 
-app.put('/teas/:id',(req,res)=>{
-    // const teaID=req.params.id
-    const tea = teaData.find(t => t.id === parseInt(req.params.id));
-    if (!tea) {
-      return res.status(404).send("Tea not found");
-    }
-    const {name,price}=req.body
-    tea.name=name
-    tea.price=price
-    res.send(200).send(tea)
-})
+app.get("/teas", (req, res) => {
+  res.status(200).send(teaData);
+});
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+  res.status(200).send(tea);
+});
 
+app.put("/teas/:id", (req, res) => {
+  // const teaID=req.params.id
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.send(200).send(tea);
+});
 
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Tea not Found");
+  }
+  teaData.splice(index, 1);
+  return res.status(200).send("deleted");
+});
 
-app.delete('/teas/:id',(req,res)=>{
-    const index=teaData.findIndex(t=> t.id===parseInt(req.params.id))
-    if (index===-1) {
-        return res.status(404).send('Tea not Found')
-    }
-    teaData.splice(index,1)
-    return res.status(200).send("deleted");
-})
-
-
-
-app.listen(port,()=>{
-    console.log('Server is running');
-    
-})
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
---
+++ MegaProject-MongoDB/vidtube/src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+
+
+const app =express()
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
+//Basic Middleware
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+//importing routes
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+//routes
+app.use("/api/v1/healthcheck",healthcheckRouter)
+
+
+export{app}
