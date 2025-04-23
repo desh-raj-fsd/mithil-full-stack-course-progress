26312d69a7f606d297b89b1f83e37cba00f18d19
Mithil Menon <mithilmenon11@gmail.com>
2025-04-23 18:21:20 +0530

    Backend MongoDB

--- MegaProject-MongoDB/vidtube/src/app.js
+++ MegaProject-MongoDB/vidtube/src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-
+import cookieParser from "cookie-parser";
 
 const app =express()
 
@@ -16,12 +16,16 @@ app.use(
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser());
+
 
 //importing routes
 import healthcheckRouter from "./routes/healthcheck.routes.js"
+import userRouter from "./routes/user.routes.js";
 
 //routes
 app.use("/api/v1/healthcheck",healthcheckRouter)
+app.use("/api/v1/users", userRouter);
 
 
 export{app}
--- MegaProject-MongoDB/vidtube/src/models/users.models.js
+++ MegaProject-MongoDB/vidtube/src/models/users.models.js
@@ -1,48 +1,91 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-  coverImage: {
-    type: String,
-  },
-  watchHistory: [
-    {
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
         type: Schema.Types.ObjectId,
-        ref:"Video"
-    }
-  ],
-  password:{
-    type:String,
-    required:[true,"password is required"],
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  refreshToken:{
-    type:String,
-  },
-},
-{timestamps:true}
+  {
+    timestamps: true,
+  }
 );
 
-export const User =mongoose.model("User", userSchema)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
